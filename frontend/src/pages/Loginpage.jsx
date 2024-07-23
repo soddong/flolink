3e5f7f8b550a51602a94f0b20acd222f1f9ffaaf
DESKTOP-MMCQPV1\SSAFY <jiheun3124@naver.com>
2024-07-23 17:37:01 +0900

    [FE] fix : css파일 module로 변경

--- frontend/src/pages/Loginpage.jsx
+++ frontend/src/pages/Loginpage.jsx
@@ -3,35 +3,35 @@ import { Button, TextField, Box, Typography } from '@mui/material';
 import logo from '../assets/logo.png';  
 import kakaoLogo from '../assets/kakao.png'; 
 import googleLogo from '../assets/google.png'; 
-import '../css/Loginpage.css';
+import LoginPageStyle from '../css/Loginpage.module.css';
 
 function LoginPage() {
   return (
-    <div className="root">
+    <div className={LoginPageStyle.root}>
       <img src={logo} alt="FLORINK" className="logo" />
       <TextField
-        className="input"
+        className={LoginPageStyle.input}
         label="전화번호 또는 아이디"
         variant="outlined"
       />
       <TextField
-        className="input"
+        className={LoginPageStyle.input}
         label="비밀번호"
         variant="outlined"
         type="password"
       />
-      <Button className="customButton" variant="contained">로그인</Button>
+      <Button className={LoginPageStyle.customButton} variant="contained">로그인</Button>
       <Box>
-        <Typography className="textLink">아이디 찾기</Typography>
-        <Typography className="textLink">비밀번호 찾기</Typography>
-        <Typography className="textLink">회원가입</Typography>
+        <Typography className={LoginPageStyle.textLink}>아이디 찾기</Typography>
+        <Typography className={LoginPageStyle.textLink}>비밀번호 찾기</Typography>
+        <Typography className={LoginPageStyle.textLink}>회원가입</Typography>
       </Box>
-      <Box className="snsContainer">
-        <Button className="snsButton" variant="contained">
-          <img src={kakaoLogo} alt="Kakao" className="snsLogo" /> 카카오로 시작
+      <Box className={LoginPageStyle.snsContainer}>
+        <Button className={LoginPageStyle.snsButton} variant="contained">
+          <img src={kakaoLogo} alt="Kakao" className={LoginPageStyle.snsLogo} /> 카카오로 시작
         </Button>
-        <Button className="snsButton" variant="contained">
-          <img src={googleLogo} alt="Google" className="snsLogo" /> 구글 계정으로 시작
+        <Button className={LoginPageStyle.snsButton} variant="contained">
+          <img src={googleLogo} alt="Google" className={LoginPageStyle.snsLogo} /> 구글 계정으로 시작
         </Button>
       </Box>
     </div>
