c90bcef8df0494462345398add9067992d682c09
address0 <soyong6624@gmail.com>
2024-07-23 20:09:44 +0900

    [FE] Design: google icon 디자인 수정 및 배경 색 지정

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -4,11 +4,11 @@ import UserStatusList from '../components/UserStatusList';
 
 function MainPage() {
   return (
-  <div>
+  <div className={style.apps}>
     <header className={`${style.displayFlex} ${style.justifyBetween}`}>
       <h1 className={`${style.margin0} ${style.fontBolder} ${style.colorE37C91} ${style.fontSize24}`}>우리는 꿀벌 가족🍯</h1>
       <div className={`${style.width30} ${style.height30} ${style.colorWhite} ${style.backgroundColorE37C91} ${style.borderRadius5} ${style.displayFlex} ${style.justifyCenter} ${style.alignCenter}`}>
-        <span className={style.materialSymbolsOutlined}>
+        <span className="material-symbols-outlined" style={{'font-variation-settings': '"FILL" 1'}}>
           notifications
         </span>
       </div>
