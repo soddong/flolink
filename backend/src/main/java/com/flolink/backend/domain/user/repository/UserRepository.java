58740da1ab732a445cdc12bdbee50a5a70d70bd9
JungHyeon Hwang <wjdgus982@naver.com>
2024-07-23 17:57:13 +0900

    [BE] feat : userEntity 및 설정

---
+++ backend/src/main/java/com/flolink/backend/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.flolink.backend.domain.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.flolink.backend.domain.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
