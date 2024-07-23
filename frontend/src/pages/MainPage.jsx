1991ec5748eef75afe73ca4770a3ccb72909d8ca
address0 <soyong6624@gmail.com>
2024-07-23 14:14:00 +0900

    [FE] feat: 메인 페이지 header 제작

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -3,7 +3,14 @@ import '../css/mainpage.css'
 function MainPage() {
   return (
   <div>
-    <h1>Hello :D</h1>
+    <header className='display-flex justify-between'>
+      <h1 className='margin-0 font-bolder color-E37C91 font-size-24'>우리는 꿀벌 가족🍯</h1>
+      <div className='width-30 height-30 color-white backgroundcolor-E37C91 border-radius-5 display-flex justify-center align-center'>
+      <span class="material-symbols-outlined">
+        notifications
+        </span>
+      </div>
+    </header>
   </div>
   )
 }
