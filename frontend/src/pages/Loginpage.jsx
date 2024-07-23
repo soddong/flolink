a07f07ce21d805f09e4da446c4d887775a25e56e
DESKTOP-MMCQPV1\SSAFY <jiheun3124@naver.com>
2024-07-23 15:56:26 +0900

    [FE] feat : loginpage css파일  분리

--- frontend/src/pages/Loginpage.jsx
+++ frontend/src/pages/Loginpage.jsx
@@ -1,89 +1,40 @@
 import React from 'react';
 import { Button, TextField, Box, Typography } from '@mui/material';
-import { styled } from '@mui/system';
 import logo from '../assets/logo.png';  
 import kakaoLogo from '../assets/kakao.png'; 
 import googleLogo from '../assets/google.png'; 
-
-const Root = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  height: '100vh',
-  backgroundColor: '#fff',
-});
-
-const Logo = styled('img')({
-  width: '100px',
-  marginBottom: '16px',
-});
-
-const Input = styled(TextField)({
-  marginBottom: '16px',
-  width: '90%',
-  maxWidth: '390px',
-});
-
-const CustomButton = styled(Button)({
-  marginBottom: '16px',
-  width: '90%',
-  maxWidth: '390px',
-  background: 'linear-gradient(to right, #ff758c, #ff7eb3)',
-  color: '#fff',
-});
-
-const SnsButton = styled(Button)({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  width: '90%',
-  maxWidth: '390px',
-  marginBottom: '16px',
-  backgroundColor: '#f5f5f5',
-  color: '#000',
-});
-
-const SnsContainer = styled(Box)({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  marginTop: '16px',
-});
-
-const TextLink = styled(Typography)({
-  color: '#9e9e9e',
-  marginBottom: '8px',
-});
+import '../css/Loginpage.css';
 
 function LoginPage() {
   return (
-    <Root>
-      <Logo src={logo} alt="FLORINK" />
-      <Input
+    <div className="root">
+      <img src={logo} alt="FLORINK" className="logo" />
+      <TextField
+        className="input"
         label="전화번호 또는 아이디"
         variant="outlined"
       />
-      <Input
+      <TextField
+        className="input"
         label="비밀번호"
         variant="outlined"
         type="password"
       />
-      <CustomButton variant="contained">로그인</CustomButton>
+      <Button className="customButton" variant="contained">로그인</Button>
       <Box>
-        <TextLink>아이디 찾기</TextLink>
-        <TextLink>비밀번호 찾기</TextLink>
-        <TextLink>회원가입</TextLink>
+        <Typography className="textLink">아이디 찾기</Typography>
+        <Typography className="textLink">비밀번호 찾기</Typography>
+        <Typography className="textLink">회원가입</Typography>
       </Box>
-      <SnsContainer>
-        <SnsButton variant="contained">
-          <img src={kakaoLogo} alt="Kakao" style={{ marginRight: '8px', width: '24px', height: '24px' }} /> 카카오로 시작
-        </SnsButton>
-        <SnsButton variant="contained">
-          <img src={googleLogo} alt="Google" style={{ marginRight: '8px', width: '24px', height: '24px' }} /> 구글 계정으로 시작
-        </SnsButton>
-      </SnsContainer>
-    </Root>
+      <Box className="snsContainer">
+        <Button className="snsButton" variant="contained">
+          <img src={kakaoLogo} alt="Kakao" className="snsLogo" /> 카카오로 시작
+        </Button>
+        <Button className="snsButton" variant="contained">
+          <img src={googleLogo} alt="Google" className="snsLogo" /> 구글 계정으로 시작
+        </Button>
+      </Box>
+    </div>
   );
 }
 
