f57b1bc39bba43c64b4578fdcb715e5f10db6723
address0 <soyong6624@gmail.com>
2024-07-23 20:51:41 +0900

    [FE] Design: user status 컴포넌트 디자인

--- frontend/src/components/ProfilePhoto.jsx
+++ frontend/src/components/ProfilePhoto.jsx
@@ -1,9 +1,12 @@
 import '../css/main.module.css'
+import Photo from '../assets/profile_dummy.jpg'
+import style from '../css/main.module.css'
 
-function ProfilePhoto () {
+function ProfilePhoto (props) {
   return (
     <div>
-      <img src="../assets/profile_dummy.jpg" alt="profile_photo" />
+      <img src={Photo} alt="profile_photo" className={`${style.width40} ${style.borderRadiusFull}`} />
+      <p className={`${style.fontSize10} ${style.margin0} ${style.textCenter} ${style.color767676}`}>{props.name}</p>
     </div>
   )
 }
--- frontend/src/components/UserStatusList.jsx
+++ frontend/src/components/UserStatusList.jsx
@@ -2,10 +2,17 @@ import style from '../css/main.module.css'
 import ProfilePhoto from './ProfilePhoto';
 
 function UserStatusList () {
+  const imageList = ['엄마', '아빠', '첫째', '둘째']
   return (
-    <div className={`${style.borderBox} ${style.marginTop15} ${style.height80} ${style.padding5_20} ${style.backgroundColorWhite70} ${style.borderRadius10} ${style.displayFlex} ${style.flexColumn} ${style.alignCenter}`}>
+    <div className={`${style.borderBox} ${style.marginTop15} ${style.height95} ${style.padding5_20} ${style.backgroundColorWhite70} ${style.borderRadius10} ${style.displayFlex} ${style.flexColumn} ${style.alignCenter}`}>
       <p className={`${style.fontSize15} ${style.margin0} ${style.fontBolder} ${style.color767676}`}>오늘 우리의 기분은?</p>
-      <ProfilePhoto />
+      <div className={`${style.displayFlex} ${style.widthFull} ${style.justifyAround} ${style.margin5_0}`}>
+        {imageList.map((image, index) => {
+          return (
+            <ProfilePhoto name={image} />
+          );
+        })}
+      </div>
     </div>
   )
 }
