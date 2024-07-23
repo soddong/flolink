693bb4d4d0829b039fc115c7afe64772b568f845
황정현 <wjdgus982@naver.com>
2024-07-24 00:55:22 +0900

    [BE] feat. 회원가입 작업 진행중 #S11P12A608-191

--- backend/src/main/java/com/flolink/backend/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/flolink/backend/domain/user/repository/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.flolink.backend.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    boolean existsByLoginId(String loginId);
 }
