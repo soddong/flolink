8d1af9d42696e48f13e9b273bfb1cbdaecbb5891
address0 <soyong6624@gmail.com>
2024-07-24 13:31:54 +0900

    [FE] fix: 화면 padding 설정 변경

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -7,17 +7,19 @@ import NavBar from '../components/NavBar';
 function MainPage() {
   return (
   <div className={style.apps}>
-    <header className={`${style.displayFlex} ${style.justifyBetween}`}>
-      <h1 className={`${style.margin0} ${style.fontBolder} ${style.colorE37C91} ${style.fontSize24}`}>우리는 꿀벌 가족🍯</h1>
-      <div className={`${style.width30} ${style.height30} ${style.colorWhite} ${style.backgroundColorE37C91} ${style.borderRadius5} ${style.displayFlex} ${style.justifyCenter} ${style.alignCenter}`}>
-        <span className="material-symbols-outlined" style={{'fontVariationSettings': '"FILL" 1'}}>
-          notifications
-        </span>
-      </div>
-    </header>
-    <Notification message='️오늘 저녁에 외식 예정. 7시까지 오세요!' />
-    <UserStatusList />
-    <PetStatusList />
+    <div className={`${style.padding30} ${style.widthFull} ${style.borderBox}`}>
+      <header className={`${style.displayFlex} ${style.justifyBetween}`}>
+        <h1 className={`${style.margin0} ${style.fontBolder} ${style.colorE37C91} ${style.fontSize24}`}>우리는 꿀벌 가족🍯</h1>
+        <div className={`${style.width30} ${style.height30} ${style.colorWhite} ${style.backgroundColorE37C91} ${style.borderRadius5} ${style.displayFlex} ${style.justifyCenter} ${style.alignCenter}`}>
+          <span className="material-symbols-outlined" style={{'fontVariationSettings': '"FILL" 1'}}>
+            notifications
+          </span>
+        </div>
+      </header>
+      <Notification message='️오늘 저녁에 외식 예정. 7시까지 오세요!' />
+      <UserStatusList />
+      <PetStatusList />
+    </div>
     <NavBar />
   </div>
   )
