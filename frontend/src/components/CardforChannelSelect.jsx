e92cc3cb7eeb9b418c87c7a3e83013b17a141396
KimSunWoong <linehero94@gmail.com>
2024-07-24 17:37:56 +0900

    [FE] feat : setting page

--- frontend/src/components/CardforChannelSelect.jsx
+++ frontend/src/components/CardforChannelSelect.jsx
@@ -2,9 +2,20 @@ import styles from '../css/channelselect.module.css'
 
 function CardforChannelSelect(props) {
     return (
-        <div className={styles.cardforselect}>
-            <h3>{props.a.title}</h3>
-            <h4>{props.a.familySize}</h4>
+        // <div className={styles.cardforselect}>
+        //     <h3>{props.a.title}</h3>
+        //     <h4>{props.a.familySize}</h4>
+        // </div>
+        <div className={styles.card}>
+            <div className={styles.img}>
+                
+            </div>
+                <div className={styles.text}>
+                    
+                    <div className={styles.h3}>{props.a.title}</div>
+                    <div className={styles.p}>명{props.a.familySize}</div>
+                    
+                </div>
         </div>
     )
 }
