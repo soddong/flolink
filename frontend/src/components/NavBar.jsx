beea9aa31da4c9c88ed3fcebb6ce6c9254a987fb
address0 <soyong6624@gmail.com>
2024-07-24 14:17:03 +0900

    [FE] design: NavBar 컴포넌트 제작 완료(추후 router 상호작용 추가 예정) #S11P12A608-156

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,17 @@
 import style from '../css/main.module.css'
+import NavBarItem from './NavBarItem';
 
 function NavBar () {
+  const NavBarSpanList = ['calendar_month', 'newsmode', 'home', 'deceased', 'account_circle']
+  const NavBarNameList = ['가족 일정', '일기장', 'Home', '기억정원','마이룸']
   return (
-    <div className={`${style.positionAbsolute} ${style.positionBottom} ${style.widthFull} ${style.height70} ${style.backgroundColorWhite70} ${style.borderBoxw} ${style.displayFlex} ${style.alignCenter} ${style.justifyCenter}`}>footer</div>
+    <div className={`${style.positionAbsolute} ${style.positionBottom} ${style.widthFull} ${style.height70} ${style.backgroundColorWhite70} ${style.borderBox} ${style.displayFlex} ${style.alignCenter} ${style.justifyBetween} ${style.padding5_20}`}>
+      {NavBarSpanList.map((item, index) => {
+        return (
+          <NavBarItem span={item} name={NavBarNameList[index]} key={index}/>
+        )
+      })}
+    </div>
   )
 }
 
