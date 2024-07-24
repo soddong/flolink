e2fe4ddfe5b8e7c8de4b6d7897bbd440e8751164
address0 <soyong6624@gmail.com>
2024-07-24 10:24:24 +0900

    [FE] feat: 다마고치 status 출력 란 제작

--- frontend/src/components/PetStatusList.jsx
+++ frontend/src/components/PetStatusList.jsx
@@ -1,8 +1,14 @@
 import style from '../css/main.module.css'
 
 function PetStatusList () {
+  const statList = ['애정도', '포만감', '체력']
+  const statusList = [0.6, 0.4, 0.8]
   return (
-    <p>this is status</p>
+    <div className={`${style.marginTop15} ${style.height80} ${style.positionRelative}`}>
+      <div className={`${style.borderWhite2px} ${style.heightFull} ${style.width150} ${style.positionAbsolute} ${style.positionRight} ${style.displayFlex} ${style.flexColumn} ${style.alignCenter}`}>
+        <p className={`${style.margin3_0} ${style.fontSize12} ${style.fontBolder} ${style.colorWhite}`}>STATS</p>
+      </div>
+    </div>
   )
 }
 
