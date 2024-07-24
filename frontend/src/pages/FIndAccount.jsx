beb646fc0b585a2f18e5de947fe6c0eabd97ec5b
DESKTOP-MMCQPV1\SSAFY <jiheun3124@naver.com>
2024-07-24 14:16:21 +0900

    [FE] feat : PwReset 생성 #S11P12A608-214

--- frontend/src/pages/FIndAccount.jsx
+++ frontend/src/pages/FIndAccount.jsx
@@ -23,12 +23,7 @@ function FindAccount() {
         >
           비밀번호 찾기
         </div>
-        <div 
-          className={`${FindAccountStyle.tab} ${activeTab === 'resetPw' ? FindAccountStyle.activeTab : ''}`}
-          onClick={() => setActiveTab('resetPw')}
-        >
-          비밀번호 재설정
-        </div>
+        
       </div>
       {activeTab === 'findId' && (
         <div className={FindAccountStyle.form}>
@@ -47,14 +42,6 @@ function FindAccount() {
           <Button variant="outlined" className={FindAccountStyle.button}>인증번호 전송</Button>
           <TextField label="인증번호 입력" variant="outlined" fullWidth className={FindAccountStyle.input} />
           <Button variant="contained" className={FindAccountStyle.submitButton}>비밀번호 찾기</Button>
-        </div>
-      )}
-      {activeTab === 'resetPw' && (
-        <div className={FindAccountStyle.form}>
-          <TextField label="기존 아이디를 입력해주세요" variant="outlined" fullWidth className={FindAccountStyle.input} />
-          <TextField label="새로운 비밀번호를 설정해주세요" variant="outlined" fullWidth className={FindAccountStyle.input} type="password" />
-          <TextField label="한번 더 입력해주세요" variant="outlined" fullWidth className={FindAccountStyle.input} type="password" />
-          <Button variant="contained" className={FindAccountStyle.submitButton}>확인</Button>
         </div>
       )}
     </div>
