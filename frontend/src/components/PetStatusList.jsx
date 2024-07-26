e583a6c6f7bc8dcda92fd1194c40da704117aad8
address0 <soyong6624@gmail.com>
2024-07-26 21:14:21 +0900

    [FE] fix: 메인 화면(오늘의 질문) 렌더링 오류 수정

--- frontend/src/components/PetStatusList.jsx
+++ frontend/src/components/PetStatusList.jsx
@@ -5,9 +5,9 @@ function PetStatusList () {
   const statusList = [60, 40, 80]
   const statusColor = ['#E37C91', '#6CCD57', '#85ABEA']
   return (
-    <div className="mt-3.5 h-20 relative">
+    <div className="mt-3.5 h-16 relative">
       <div className="h-full border-2 border-solid border-white w-40 absolute right-0 flex flex-col items-center">
-        <p className="mt-1 text-xs font-bold text-white">STATS</p>
+        <p className="text-xs font-bold text-white">STATS</p>
         {statList.map((stat, index) => {
           return (
             <PetStatusListItem name={stat} value={statusList[index]} color={statusColor[index]} key={index}/>
