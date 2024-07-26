bec78a916f81c6a10b4ee864ffea37d3902205a4
황정현 <wjdgus982@naver.com>
2024-07-26 23:34:23 +0900

    [BE] feat : JwtUtil에 temp_user_id 추가 #S11P12A608-194

---
+++ backend/src/main/java/com/flolink/backend/global/util/JwtUtil.java
@@ -0,0 +1,15 @@
+package com.flolink.backend.global.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtUtil {
+
+
+    public int tempReturnUserId(){
+
+        return 1;
+    }
+}
