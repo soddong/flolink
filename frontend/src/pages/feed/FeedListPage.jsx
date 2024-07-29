db24c73c43790379dec894240a6d26ddd536750f
DESKTOP-MMCQPV1\SSAFY <jiheun3124@naver.com>
2024-07-29 18:02:40 +0900

    [FE] fix : feed 수정 #S11P12A608-378

--- frontend/src/pages/feed/FeedListPage.jsx
+++ frontend/src/pages/feed/FeedListPage.jsx
@@ -36,17 +36,17 @@ const FeedListPage = () => {
     (
       <div className="min-h-screen bg-custom-gradient">
           <img src={logo} alt="Logo" className="mx-auto h-20" />
-          <div className="p-4">
+          <div className="p-4 overflow-auto">
             <FeedList feeds={feeds} />
           </div>
       </div>
     );
   }else{
     return (
-        <div className="min-h-screen bg-custom-gradient relative">
+        <div className="w-full h-full min-h-screen bg-custom-gradient relative">
           <img src={logo} alt="Logo" className="mx-auto h-20" />
           
-          <div className="p-4">
+          <div className="p-4 flex flex-col h-3/4">
             <div className="mb-4">
               <input 
                 type="text" 
@@ -54,12 +54,14 @@ const FeedListPage = () => {
                 className="w-full p-2 border border-gray-300 rounded-md bg-transparent"
               />
             </div>
-            <FeedList feeds={feeds} />
+            <div className="flex-1 overflow-auto">
+              <FeedList feeds={feeds} />
+            </div>
           </div>
           <button
             className="border border-gray-400 text-gray rounded-full w-12 h-12 flex items-center justify-center text-2xl fixed"
             onClick={() => navigate('/feedcreate')}
-            style={{ bottom: '2rem', right: '7rem' }}
+            style={{ bottom: '2rem', right: '2rem' }}
           >
             +
           </button>
