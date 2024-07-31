13b2f7b8d30f93232dbc47135f992adc40f9cb89
최소현 <aledjdygus9300@gmail.com>
2024-07-31 09:05:04 +0900

    [BE] feat : 애완식물 정보 조회 #S11P12A608-373

---
+++ backend/src/main/java/com/flolink/backend/domain/plant/repository/PlantRepository.java
@@ -0,0 +1,11 @@
+package com.flolink.backend.domain.plant.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.flolink.backend.domain.plant.entity.Plant;
+
+public interface PlantRepository extends JpaRepository<Plant, Integer> {
+	Optional<Plant> findByRoomRoomId(Integer roomId);
+}
