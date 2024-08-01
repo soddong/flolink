9cc9f909f21bce17e5ba9091e3226af1f384e1ed
address0 <soyong6624@gmail.com>
2024-08-01 15:04:59 +0900

    [FE] feat: 가족정원 페이지 Head/Footer 구현  #S11P12A608-409

--- frontend/src/pages/garden/FamilyGardenPage.jsx
+++ frontend/src/pages/garden/FamilyGardenPage.jsx
@@ -1,6 +1,18 @@
+import NavBar from '../../components/common/nav_bar/NavBar';
+import Background from '../../assets/garden/garden_background.png'
+import Header from '../../assets/garden/panel_head.png'
+
 function FamilyGardenPage () {
   return (
-    <div>hello</div>
+    <div className="w-full h-full box-border relative">
+      <img src={Background} alt="garden_background"
+      className='w-full h-full absolute' />
+      <div className='relative flex justify-center w-64'>
+        <img src={Header} alt="panel_head" className='absolute' />
+        <h1 className='text-3xl font-bold text-red-900 absolute top-7'>기억정원🌷</h1>
+      </div>
+      <NavBar />
+    </div>
   )
 }
 
