370a0bf6852a836eb3a2c0f20d830c77a76a9188
최소현 <aledjdygus9300@gmail.com>
2024-08-02 15:58:29 +0900

    [BE] fix : 기억정원 리스트 조회시 레벨도 전달하도록 수정 #S11P12A608-454

--- backend/src/main/java/com/flolink/backend/domain/plant/repository/PlantHistoryRepository.java
+++ backend/src/main/java/com/flolink/backend/domain/plant/repository/PlantHistoryRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.flolink.backend.domain.plant.entity.Plant;
 import com.flolink.backend.domain.plant.entity.PlantExpHistory;
 
 public interface PlantHistoryRepository extends JpaRepository<PlantExpHistory, Integer> {
 
-	@Query("SELECT h FROM PlantExpHistory h WHERE h.plant.plantId = :plantId AND FUNCTION('YEAR', h.dateMonth) = :year")
-	List<PlantExpHistory> findByPlantIdAndYear(Integer plantId, Integer year);
+	@Query("SELECT h FROM PlantExpHistory h WHERE h.plant = :plant AND FUNCTION('YEAR', h.dateMonth) = :year")
+	List<PlantExpHistory> findByPlantIdAndYear(Plant plant, Integer year);
 
 }
