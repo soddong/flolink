1fa3a4cd35cf7589b50e4ff82b48ae3a655ebb90
JungHyeon Hwang <wjdgus982@naver.com>
2024-08-05 17:50:21 +0900

    [BE] fix : delete쿼리 오류 수정

--- backend/src/main/java/com/flolink/backend/domain/auth/repository/AuthRepository.java
+++ backend/src/main/java/com/flolink/backend/domain/auth/repository/AuthRepository.java
@@ -3,6 +3,8 @@ package com.flolink.backend.domain.auth.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.flolink.backend.domain.auth.entity.Auth;
@@ -12,7 +14,8 @@ public interface AuthRepository extends JpaRepository<Auth, Integer> {
 
 	Optional<Auth> findByTel(String tel);
 
-	void deleteByTel(String tel);
+	@Query("delete from Auth a where a.tel = :tel")
+	void deleteByTel(@Param("tel") String tel);
 
 	boolean existsByTel(String tel);
 
