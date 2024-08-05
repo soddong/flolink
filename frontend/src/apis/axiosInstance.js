aaa4f590f3f7413dba2196a823ed65789c783b0d
KimSunWoong <linehero94@gmail.com>
2024-08-05 14:29:17 +0900

    [FE] fix : 백이랑 연동중

---
+++ frontend/src/apis/axiosInstance.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+// import setAuthorization from './setAuthorization';
+// import refresh from './refresh';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const axiosRequestConfig = {
+  baseURL: BASE_URL,
+};
+
+const axiosWithCredentialConfig = {
+  baseURL: BASE_URL,
+  withCredentials: true,
+};
+
+console.log(BASE_URL)
+// 일반 요청을 위한 인스턴스
+export const axiosCommonInstance = axios.create(axiosRequestConfig);
+
+// 인증이 필요한 요청을 위한 인스턴스
+export const axiosWithCredentialInstance = axios.create(axiosWithCredentialConfig);
+
+// // 요청 인터셉터 설정
+// axiosCommonInstance.interceptors.request.use(setAuthorization);
+// axiosWithCredentialInstance.interceptors.request.use(setAuthorization);
+
+// // 응답 인터셉터 설정 (토큰 갱신)
+// axiosWithCredentialInstance.interceptors.response.use(
+//   response => response,
+//   error => refresh(error)
+// );
