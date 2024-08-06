e52b7b8ce2cd8817b54b4e0103ba8046eeafffcf
황정현 <wjdgus982@naver.com>
2024-08-07 00:41:54 +0900

    [BE] feat : pw찾기 -> 임시비밀번호 발급(문자)로 변경

--- backend/src/main/java/com/flolink/backend/global/auth/service/AuthService.java
+++ backend/src/main/java/com/flolink/backend/global/auth/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.flolink.backend.global.auth.service;
 
 import com.flolink.backend.global.auth.dto.request.CheckAuthRequest;
+import com.flolink.backend.global.auth.dto.request.ResetPassword;
 import com.flolink.backend.global.auth.dto.response.SuccessTokenResponse;
 
 public interface AuthService {
@@ -9,4 +10,5 @@ public interface AuthService {
 
 	SuccessTokenResponse checkAuthenticationNumber(CheckAuthRequest checkAuthRequest);
 
+    void sendTempPassword(ResetPassword resetPassword);
 }
