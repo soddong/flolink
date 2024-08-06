de9ee9432e6b0cc602c37e2b0011fa7507971484
최소현 <aledjdygus9300@gmail.com>
2024-08-06 13:22:07 +0900

    [BE] feat : 포인트 (결제 아이템) 구현 #S11P12A608-471

--- backend/src/main/java/com/flolink/backend/domain/payment/dto/request/PaymentCreateRequest.java
+++ backend/src/main/java/com/flolink/backend/domain/payment/dto/request/PaymentCreateRequest.java
@@ -2,12 +2,13 @@ package com.flolink.backend.domain.payment.dto.request;
 
 import java.math.BigDecimal;
 
+import com.flolink.backend.domain.payment.entity.PaymentItem;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PaymentCreateRequest {
-	private String orderName;
-	private BigDecimal amount;
+	private Integer pointId;
 }
