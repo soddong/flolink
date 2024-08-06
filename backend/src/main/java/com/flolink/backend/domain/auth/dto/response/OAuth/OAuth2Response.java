d52a0768dbe3d344775a07499f7953316f3d24ed
JungHyeon Hwang <wjdgus982@naver.com>
2024-08-06 13:55:28 +0900

    [BE] OAuth2 로그인 연동을 위한 수정 #S11P12A608-475

--- backend/src/main/java/com/flolink/backend/domain/auth/dto/response/OAuth/OAuth2Response.java
+++ backend/src/main/java/com/flolink/backend/domain/auth/dto/response/OAuth/OAuth2Response.java
@@ -1,16 +1,14 @@
 package com.flolink.backend.domain.auth.dto.response.OAuth;
 
-import com.flolink.backend.domain.user.entity.enumtype.RoleType;
-
-
 public interface OAuth2Response {
-
 
 	//제공자 (Ex. naver, google, ...)
 	String getProvider();
 
 	//제공자에서 발급해주는 아이디(번호)
 	String getProviderId();
+
+	String getEmail();
 
 	//사용자 실명 (설정한 이름)
 	String getName();
