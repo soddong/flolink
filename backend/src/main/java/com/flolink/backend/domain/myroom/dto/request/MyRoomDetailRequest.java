b60faf9eb6211acab106c1aa24d93032667dd11c
최소현 <aledjdygus9300@gmail.com>
2024-08-07 16:21:52 +0900

    [BE] fix : DTO에 하나만 있을때 Builder사용시 에러나는 문제 해결

--- backend/src/main/java/com/flolink/backend/domain/myroom/dto/request/MyRoomDetailRequest.java
+++ backend/src/main/java/com/flolink/backend/domain/myroom/dto/request/MyRoomDetailRequest.java
@@ -1,12 +1,10 @@
 package com.flolink.backend.domain.myroom.dto.request;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
 public class MyRoomDetailRequest {
 	private Integer userRoomId;
 }
