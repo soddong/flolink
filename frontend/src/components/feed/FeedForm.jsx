951dfbb145f8c589e8e989a2f1cbb3f6b1ee0735
DESKTOP-MMCQPV1\SSAFY <jiheun3124@naver.com>
2024-08-07 21:52:48 +0900

    [FE] feat : 피드 API 연동 #S11P12A608-523

--- frontend/src/components/feed/FeedForm.jsx
+++ frontend/src/components/feed/FeedForm.jsx
@@ -2,10 +2,12 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { addFeed } from '../../service/Feed/feedApi';
-const FeedForm = ({feed}) => {
+import { useNavigate } from 'react-router-dom';
+const FeedForm = ({feed, roomId}) => {
   const [content, setContent] = useState('');
   const [images, setImages] = useState([]);
   const textareaRef = useRef(null);
+  const navigate = useNavigate();
   
   console.log("feed_form");
   useEffect(() => {
@@ -35,19 +37,13 @@ const FeedForm = ({feed}) => {
     setImages((prevImages) => [...prevImages, ...files]);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // 피드 제출 로직 추가
     console.log('내용:', content);
     console.log('이미지들:', images);
-    const feedcontent = {
-      roomId: roomId,
-      content: content,
-      images: images
-    }
-    addFeed(feedcontent).then(()=>{
-      Navigate('/feedlist');
-    })
+    await addFeed(roomId, images, content)
+    navigate('/feedlist');
   };
 
   return (
