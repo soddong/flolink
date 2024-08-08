9535fde7a47b88bf184f65132274f4f2142fb1a0
JungHyeon Hwang <wjdgus982@naver.com>
2024-08-08 14:49:26 +0900

    [BE] fix: userInfoResponse 수정

--- backend/src/main/java/com/flolink/backend/domain/user/dto/request/StatusMessageRequest.java
+++ backend/src/main/java/com/flolink/backend/domain/user/dto/request/StatusMessageRequest.java
@@ -1,11 +1,15 @@
 package com.flolink.backend.domain.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StatusMessageRequest {
-    private String statusMessage;
+	private String statusMessage;
 
 }
--- backend/src/main/java/com/flolink/backend/domain/user/dto/response/UserInfoResponse.java
+++ backend/src/main/java/com/flolink/backend/domain/user/dto/response/UserInfoResponse.java
@@ -2,8 +2,10 @@ package com.flolink.backend.domain.user.dto.response;
 
 import java.math.BigDecimal;
 
+import com.flolink.backend.domain.user.entity.User;
 import com.flolink.backend.domain.user.entity.enumtype.EmotionType;
 import com.flolink.backend.domain.user.entity.enumtype.ProfileType;
+import com.flolink.backend.domain.user.entity.enumtype.RoleType;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -13,10 +15,25 @@ import lombok.Setter;
 @Setter
 @Builder
 public class UserInfoResponse {
+	private Integer myRoomId;
 	private String nickname;
 	private BigDecimal point;
+	private RoleType role;
 	private ProfileType profile;
 	private EmotionType emotion;
+	private String statusMessage;
+
+	public static UserInfoResponse fromEntity(User User) {
+		return UserInfoResponse.builder()
+			.myRoomId(User.getMyRoom().getMyRoomId())
+			.nickname(User.getNickname())
+			.point(User.getPoint())
+			.role(User.getRole())
+			.profile(User.getProfile())
+			.emotion(User.getEmotion())
+			.statusMessage(User.getStatusMessage())
+			.build();
+	}
 }
 
 
