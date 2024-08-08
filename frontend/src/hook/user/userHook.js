9a59ba737db67b7dc1d9406058c168f3228fcc16
KimSunWoong <linehero94@gmail.com>
2024-08-08 20:09:37 +0900

    [FE] feat : 설정 페이지 최종

--- frontend/src/hook/user/userHook.js
+++ frontend/src/hook/user/userHook.js
@@ -1,9 +1,16 @@
 import { useQuery } from 'react-query';
-import { fetchInventory } from '../../service/user/userApi';
+import { fetchInventory, getMyInfo } from '../../service/user/userApi';
 
 export const useInventory = () => {
     return useQuery({
         queryKey: ['inventory'],
         queryFn: fetchInventory,
     });
+}
+
+export const useMyInfo = () => {
+    return useQuery({
+        queryKey: ['myinfo'],
+        queryFn: getMyInfo,
+    })
 }
