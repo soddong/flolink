dda93e4abcbcb0f89f33b47f4dd8bd73619d6689
박동환 <srpark13@naver.com>
2024-08-12 00:16:12 +0900

    [BE] fix : fcm 키 필드화

--- backend/src/main/java/com/flolink/backend/global/config/FcmConfig.java
+++ backend/src/main/java/com/flolink/backend/global/config/FcmConfig.java
@@ -1,10 +1,10 @@
 package com.flolink.backend.global.config;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -14,26 +14,21 @@ import com.google.firebase.messaging.FirebaseMessaging;
 @Configuration
 public class FcmConfig {
 
+	private final ClassPathResource firebaseResource = new ClassPathResource(
+		"flolint-firebase-adminsdk-w9h2b-6390b9781c.json");
+
 	@Bean
-	public FirebaseApp firebaseApp() throws IOException {
-		try {
-			FileInputStream serviceAccount = new FileInputStream(
-				"./src/main/resources/flolint-firebase-adminsdk-w9h2b-6390b9781c.json");
+	FirebaseApp firebaseApp() throws IOException {
+		FirebaseOptions options = FirebaseOptions.builder()
+			.setCredentials(GoogleCredentials.fromStream(
+				firebaseResource.getInputStream()))
+			.build();
 
-			FirebaseOptions options = FirebaseOptions
-				.builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.build();
-			return FirebaseApp.initializeApp(options);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-
+		return FirebaseApp.initializeApp(options);
 	}
 
 	@Bean
-	public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
-		return FirebaseMessaging.getInstance(firebaseApp);
+	FirebaseMessaging firebaseMessaging() throws IOException {
+		return FirebaseMessaging.getInstance(firebaseApp());
 	}
 }
