afb803d1d7b6c1566b96e014ce1f70851129fd45
박동환 <srpark13@naver.com>
2024-08-11 22:58:37 +0900

    [FE] fix : fcm 에러 수정

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,17 @@ import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const client = new QueryClient();
-
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/firebase-messaging-sw.js')
+      .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+          console.error('Service Worker registration failed:', error);
+      });
+  });
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={client}>
     <App />
--- frontend/src/sw.js
+++ frontend/src/sw.js
@@ -10,45 +10,6 @@ clientsClaim();
 
 
 
-import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
-// Firebase 구성 객체를 사용해 Firebase를 초기화합니다.
-const firebaseConfig = {
-  apiKey: "AIzaSyAEE25E246QCHn-D7zPsHCaii1Fj-j8Vko",
-  authDomain: "flolint.firebaseapp.com",
-  projectId: "flolint",
-  storageBucket: "flolint.appspot.com",
-  messagingSenderId: "83845980646",
-  appId: "1:83845980646:web:e1b2f8b93178e61d5ff627",
-  measurementId: "G-K0GBPS0YQM"
-};
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const messaging = getMessaging(firebaseApp);
-
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('sync', (event) => {
-  if (event.tag === 'sync-location') {
-    event.waitUntil(syncLocation());
-  }
-});
-
-
-async function syncLocation() {
-  try {
-    const position = await getPosition();
-    const { latitude, longitude } = position.coords;
-    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-  } catch (error) {
-    console.error('Error getting location:', error);
-  }
-}
 
 function getPosition() {
   return new Promise((resolve, reject) => {
