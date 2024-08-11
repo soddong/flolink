728ab14186585733bfc2aa894fd9166d4c152a02
박동환 <srpark13@naver.com>
2024-08-12 00:06:05 +0900

    [BE] fix : fcm 키 경로 재설정

--- backend/src/main/java/com/flolink/backend/global/config/FcmConfig.java
+++ backend/src/main/java/com/flolink/backend/global/config/FcmConfig.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -14,16 +13,23 @@ import com.google.firebase.messaging.FirebaseMessaging;
 
 @Configuration
 public class FcmConfig {
+
 	@Bean
 	public FirebaseApp firebaseApp() throws IOException {
-		FileInputStream serviceAccount = new FileInputStream(
-			String.valueOf(ResourceUtils.getFile("src/main/resources/flolint-firebase-adminsdk-w9h2b-6390b9781c.json"))
-		);
-		FirebaseOptions options = FirebaseOptions
-			.builder()
-			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-			.build();
-		return FirebaseApp.initializeApp(options);
+		try {
+			FileInputStream serviceAccount = new FileInputStream(
+				"./src/main/resources/flolint-firebase-adminsdk-w9h2b-6390b9781c.json");
+
+			FirebaseOptions options = FirebaseOptions
+				.builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
+			return FirebaseApp.initializeApp(options);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 
 	@Bean
