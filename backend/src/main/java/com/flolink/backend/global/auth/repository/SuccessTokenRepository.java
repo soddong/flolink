ec92040a695f5baa6d32cc7a5895f35f0165568a
JungHyeon Hwang <wjdgus982@naver.com>
2024-08-12 15:23:36 +0900

    [BE] fix: user 아이디 및 비밀번호 찾기 오류 수정

--- backend/src/main/java/com/flolink/backend/global/auth/repository/SuccessTokenRepository.java
+++ backend/src/main/java/com/flolink/backend/global/auth/repository/SuccessTokenRepository.java
@@ -3,6 +3,9 @@ package com.flolink.backend.global.auth.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.flolink.backend.global.auth.entity.SuccessToken;
@@ -13,5 +16,11 @@ public interface SuccessTokenRepository extends JpaRepository<SuccessToken, Inte
 	Optional<SuccessToken> findByToken(String token);
 
 	void deleteByToken(String token);
+
+	boolean existsByTel(String tel);
+
+	@Modifying
+	@Query("delete from SuccessToken a where a.tel = :tel")
+	void deleteByTel(@Param("tel") String tel);
 
 }
