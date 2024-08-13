ad5b535b8c95cd257ad222f282bcdf644f8e9d12
황정현 <wjdgus982@naver.com>
2024-08-13 23:44:18 +0900

    [BE] fix: SecurityConfig

--- backend/src/main/java/com/flolink/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/flolink/backend/global/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
 				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 					CorsConfiguration configuration = new CorsConfiguration();
 
-					configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));//프론트앤드 서버
+					configuration.setAllowedOrigins(Collections.singletonList("https://i11a608.p.ssafy.io"));//프론트앤드 서버
 					configuration.setAllowedMethods(Collections.singletonList("*"));
 					configuration.setAllowCredentials(true);
 					configuration.setAllowedHeaders(Collections.singletonList("*"));
