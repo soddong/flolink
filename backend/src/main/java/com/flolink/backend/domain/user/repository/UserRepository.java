dc8e02d572893c44e3e08386e629337daff3133d
황정현 <wjdgus982@naver.com>
2024-08-15 00:54:16 +0900

    [BE] feat: rankResponse에 Profile, Emotion 추가 #S11P12A608-655

--- backend/src/main/java/com/flolink/backend/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/flolink/backend/domain/user/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.flolink.backend.domain.user.repository;
 
 import java.util.Optional;
 
+import com.flolink.backend.domain.user.dto.response.UserProfileResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.flolink.backend.domain.user.entity.User;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
@@ -24,6 +26,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	@Query("SELECT u.loginId from User u where u.userId=:userId and u.useYn=true ")
 	String findLoginIdByUserId(int userId);
 
+	@Query("SELECT new com.flolink.backend.domain.user.dto.response.UserProfileResponse(u.profile, u.emotion) " +
+			"FROM User u WHERE u.userId = :userId AND u.useYn = true")
+	Optional<UserProfileResponse> findUserProfileByUserId(@Param("userId") Integer userId);
 }
 
 
