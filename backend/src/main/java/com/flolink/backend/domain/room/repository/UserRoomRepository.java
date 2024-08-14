e8c704db8f65d65ea22f51bd75a02b9840d9faf0
박동환 <srpark13@naver.com>
2024-08-15 02:55:04 +0900

    [BE] fix : userRoom 쿼리 구체화

--- backend/src/main/java/com/flolink/backend/domain/room/repository/UserRoomRepository.java
+++ backend/src/main/java/com/flolink/backend/domain/room/repository/UserRoomRepository.java
@@ -3,6 +3,8 @@ package com.flolink.backend.domain.room.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.flolink.backend.domain.room.entity.Room;
@@ -15,7 +17,8 @@ public interface UserRoomRepository extends JpaRepository<UserRoom, Integer> {
 
 	Optional<UserRoom> findUserRoomByUserRoomId(Integer userRoomId);
 
-	Optional<UserRoom> findByUserUserIdAndRoomRoomId(Integer userId, Integer roomId);
+	@Query("SELECT UserRoom u from UserRoom where u.user.userId=:userId and u.room.roomId=:roomId ")
+	Optional<UserRoom> findByUserUserIdAndRoomRoomId(@Param("userId") Integer userId, @Param("roomId") Integer roomId);
 
 	boolean existsByUserUserIdAndRoomRoomId(Integer userId, Integer roomId);
 }
