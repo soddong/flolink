dbdca8cc434c1b863827bb44f726440688c6f0ca
박동환 <srpark13@naver.com>
2024-08-15 15:34:42 +0900

    [BE] feat : userRoom 개수 제한

--- backend/src/main/java/com/flolink/backend/domain/room/repository/UserRoomRepository.java
+++ backend/src/main/java/com/flolink/backend/domain/room/repository/UserRoomRepository.java
@@ -1,5 +1,6 @@
 package com.flolink.backend.domain.room.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +14,10 @@ import com.flolink.backend.domain.user.entity.User;
 
 @Repository
 public interface UserRoomRepository extends JpaRepository<UserRoom, Integer> {
+
+	@Query("SELECT ur from UserRoom ur where ur.user.userId=:userId ORDER BY ur.room.roomId DESC LIMIT 5 ")
+	List<UserRoom> findAllByUserUserId(@Param("userId") final Integer userId);
+
 	Optional<UserRoom> findByUserAndRoom(User user, Room room);
 
 	Optional<UserRoom> findUserRoomByUserRoomId(Integer userRoomId);
